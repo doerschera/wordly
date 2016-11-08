ea7bf7eb0928e0865da924f4223dfacf1cfb2100
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-11-08 14:33:12 -0600

    mysql connection

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  port: 3000,
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'wordly'
+});
+
+connection.connect(function(err) {
+  if(err) {
+    console.error('error connecting: '+err.stack);
+    return;
+  }
+})
+
+module.exports(connection);
