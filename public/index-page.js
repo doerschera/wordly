e0b6839c5057bdb4328e7267667bd38a77350fdb
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-11-09 21:06:24 -0600

    filter show hide

---
+++ public/index-page.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  // filter dropdown show/hide
+  var filter = false;
+
+  $('.dropdown').on('click', function() {
+    if(!filter) {
+      $('#filter').slideDown(function() {
+        filter = true;
+        $('#droparrow').html('arrow_drop_up');
+      });
+    } else {
+      $('#filter').slideUp(function() {
+        filter = false;
+        $('#droparrow').html('arrow_drop_down');
+      });
+    }
+  })
+})
